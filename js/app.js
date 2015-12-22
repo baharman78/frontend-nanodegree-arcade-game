0dbd77333e7042ec9e748538354fc272886df66e
baharman78 <baharman78@users.noreply.github.com>
2015-12-22 12:42:06 -0500

    - Implemented changes based on Review.  Instead of   making Player a subclass of Enemy, I have made Player   and Enemy a subclass of GameEntity.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+// Default for all game entities
+var GameEntity = function(){
+  this.x = 0;
+  this.y = 0;
+  this.spd = 0;
+  this.sprite = "";
+};
+
 // Enemies our player must avoid
 var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
@@ -5,11 +13,11 @@ var Enemy = function(x, y) {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    GameEntity.call(this);
     this.x = x;
     this.y = 83*y-20;
     //player flag is used to let update use logic based
     //on whether Object is player or enemy
-    this.player = 0;
     //Enemy speed tells enemies how fast to progress across
     //screen with fluid movement
     this.spd = mathRandomRange(200, 400);
@@ -17,25 +25,47 @@ var Enemy = function(x, y) {
 };
 
 var Player = function(x, y) {
-  Enemy.call(this, x, y);
+  GameEntity.call(this);
   this.x = 101*x;
   this.y = 80*y-20;
   //player flag is used to let update use logic based
   //on whether Object is player or enemy
-  this.player = 1;
   //player speed is based on grid movement
   //ex. spd 2 moves two grids instead of 1 whenever key arrow
   //  key is pressed
   this.spd = 1;
   this.sprite = "images/char-boy.png";
 };
-Player.prototype = Object.create(Enemy.prototype);
-Player.prototype.contstructor = Player;
+
+var mathRandomRange = function(min, max){
+  return (max - min)*Math.random() + min;
+};
+
+//pseudoclassical inheritance
+//subClass will inherit from superClass
+//function was copied from Udacity Object-Oriented Javascript Notes
+//https://www.udacity.com/course/viewer#!/c-ud015
+var inherit = function(subClass, superClass) {
+  subClass.prototype = Object.create(superClass.prototype);
+  subClass.prototype.constructor = subClass;
+};
+
+inherit(Player, GameEntity);
+inherit(Enemy, GameEntity);
+// Draw the enemy or player on the screen, required method for game
+
+GameEntity.prototype.render = function(){
+  if(this.sprite !== ""){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+};
+
+GameEntity.prototype.update = function(){
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    if(this.player === 0){
       if(this.x > 503){
         this.x = -98;
         this.spd = mathRandomRange(200, 400);
@@ -43,26 +73,14 @@ Enemy.prototype.update = function(dt) {
       else{
         this.x = this.x + this.spd*dt;
       }
-    }
-
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-
-var mathRandomRange = function(min, max){
-  return (max - min)*Math.random() + min;
-};
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
 Player.prototype.handleInput = function(key){
   if(key === "left" && this.x > 0){
   this.x = this.x - 101*this.spd;
@@ -84,8 +102,10 @@ var allEnemies = [];
 for(var i = 1; i < 4 ; i++){
   allEnemies.push(new Enemy(26, i));
 }
+
 // Place the player object in a variable called player
 var player = new Player(0, 5);
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
