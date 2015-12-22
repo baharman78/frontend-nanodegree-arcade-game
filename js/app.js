e506fb4c11042e7b6c9bc05ab9e3d9dbd885fe0b
baharman78 <baharman78@users.noreply.github.com>
2015-12-22 12:55:56 -0500

    - Auto indented code

--- js/app.js
+++ js/app.js
@@ -8,20 +8,20 @@ var GameEntity = function(){
 
 // Enemies our player must avoid
 var Enemy = function(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    GameEntity.call(this);
-    this.x = x;
-    this.y = 83*y-20;
-    //player flag is used to let update use logic based
-    //on whether Object is player or enemy
-    //Enemy speed tells enemies how fast to progress across
-    //screen with fluid movement
-    this.spd = mathRandomRange(200, 400);
-    this.sprite = 'images/enemy-bug.png';
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  GameEntity.call(this);
+  this.x = x;
+  this.y = 83*y-20;
+  //player flag is used to let update use logic based
+  //on whether Object is player or enemy
+  //Enemy speed tells enemies how fast to progress across
+  //screen with fluid movement
+  this.spd = mathRandomRange(200, 400);
+  this.sprite = 'images/enemy-bug.png';
 };
 
 var Player = function(x, y) {
@@ -66,16 +66,16 @@ GameEntity.prototype.update = function(){
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-      if(this.x > 503){
-        this.x = -98;
-        this.spd = mathRandomRange(200, 400);
-      }
-      else{
-        this.x = this.x + this.spd*dt;
-      }
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  if(this.x > 503){
+    this.x = -98;
+    this.spd = mathRandomRange(200, 400);
+  }
+  else{
+    this.x = this.x + this.spd*dt;
+  }
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 };
 
 // Now write your own player class
@@ -83,16 +83,16 @@ Enemy.prototype.update = function(dt) {
 // a handleInput() method.
 Player.prototype.handleInput = function(key){
   if(key === "left" && this.x > 0){
-  this.x = this.x - 101*this.spd;
+    this.x = this.x - 101*this.spd;
   }
   else if(key === "right" && this.x < 4*101){
-  this.x = this.x + 101*this.spd;
+    this.x = this.x + 101*this.spd;
   }
   else if(key === "up" && this.y > 0){
-  this.y = this.y - 80*this.spd;
+    this.y = this.y - 80*this.spd;
   }
   else if(key === "down" && this.y < 80*4){
-  this.y = this.y + 80*this.spd;
+    this.y = this.y + 80*this.spd;
   }
 };
 
@@ -109,12 +109,12 @@ var player = new Player(0, 5);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
